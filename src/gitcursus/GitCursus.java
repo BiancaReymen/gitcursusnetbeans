bdd3d58c3c7d94dd330d281f164aa1b3baf5aa8c
Bianca Reymen <Bianca.Reymen@pandora.be>
2018-04-12 15:43:52 +0200

    hallo iedereen

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -3,7 +3,7 @@ package gitcursus;
 public class GitCursus {
 
     public static void main(String[] args) {
-        System.out.println("Bonjour");
+        System.out.println("Hallo iedereen");
         
     }
     
